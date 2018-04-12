88471d3d772fa8e6b8a9dee693e4632254aee323
neerajsarna <sarna@mathcces.rwth-aachen.de>
2018-04-12 13:04:17 +0200

    added data saving routines: Inflow

---
+++ write_result.m
@@ -0,0 +1,7 @@
+function write_result(result,filename)
+dlmwrite(filename,result(1).X','delimiter','\t','precision',10);
+
+for j = 1 : length(result)
+    dlmwrite(filename,result(j).sol','-append','delimiter','\t','precision',10);
+end
+end
