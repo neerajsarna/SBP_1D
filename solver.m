faf60f063a944b4ee54098f43e8b0603a883ace8
neerajsarna <sarna@mathcces.rwth-aachen.de>
2018-04-16 11:51:55 +0200

    added 199

--- solver.m
+++ solver.m
@@ -237,7 +237,7 @@ while t < par.t_end
                         'sol',U, ...
                         'P',PX, ...
                         'h',h);
-        output_filename = strcat('result_wall/result_Reference/inflow_t_',num2str(t),'_points_',num2str(par.n),'_neqn_');
+        output_filename = strcat('result_Inflow/result_Reference/inflow_t_',num2str(t),'_points_',num2str(par.n),'_neqn_');
         output_filename = strcat(output_filename,num2str(par.n_eqn),'.txt');
         write_result(output,output_filename);
     end
