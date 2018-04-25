bc6bf7d479a151b0d301061b46db1d2d316be471
neerajsarna <sarna@mathcces.rwth-aachen.de>
2018-04-25 13:18:13 +0200

    added result hc

--- job_serial.sh
+++ job_serial.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
  
 ### Job name
-#BSUB -J "MATLAB_ARRAY[55]"
+#BSUB -J "MATLAB_ARRAY[4,8,12,16,20,24,3,7,11,15,19,23,27,55]"
  
 ### File / path where STDOUT will be written, the %J is the job id
-#BSUB -o log_files/solving_inflow2D_%I
+#BSUB -o log_files/solving_HC2D_%I
  
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute,
@@ -12,7 +12,7 @@
 #BSUB -W 300
  
 ### Request memory you need for your job in MB
-#BSUB -M 25000
+#BSUB -M 5000
  
 ### Change to the work directory
 cd /home/ns179556/SBP_1D/
@@ -23,7 +23,7 @@ module load matlab
  
  
 # start non-interactive batch job
-matlab -singleCompThread -nodisplay -nodesktop -nosplash -logfile log_files/solver_inflow2D_$LSB_JOBINDEX.log <<EOF
-run solve_inflow2D($LSB_JOBINDEX);
+matlab -singleCompThread -nodisplay -nodesktop -nosplash -logfile log_files/solver_HC2D_$LSB_JOBINDEX.log <<EOF
+run solve_HC2D($LSB_JOBINDEX);
 quit();
 EOF
