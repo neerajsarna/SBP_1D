86e4860721fd4c919fba46eda2b5f3a488ecbc66
neerajsarna <sarna@mathcces.rwth-aachen.de>
2018-05-29 14:30:31 +0200

    results HC new

--- job_serial.sh
+++ job_serial.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
  
 ### Job name
-#BSUB -J "MATLAB_ARRAY[4,8,12,16,20,24,3,7,11,15,19,23,27,55]"
+#BSUB -J "MATLAB_ARRAY[55]"
  
 ### File / path where STDOUT will be written, the %J is the job id
 #BSUB -o log_files/solving_HC2D_%I
@@ -9,10 +9,10 @@
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute,
 ### that means for 80 minutes you could also use this: 1:20
-#BSUB -W 20:00
+#BSUB -W 22:00
  
 ### Request memory you need for your job in MB
-#BSUB -M 5000
+#BSUB -M 3000
  
 ### Change to the work directory
 cd /home/ns179556/SBP_1D/
