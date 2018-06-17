37c5621d7966e8d12e41a5f7158f46addf3c37bb
neerajsarna <sarna@mathcces.rwth-aachen.de>
2018-06-17 11:47:53 +0200

    added results for T = 1, harmonic heat inflow

--- job_serial.sh
+++ job_serial.sh
@@ -4,15 +4,15 @@
 #BSUB -J "MATLAB_ARRAY[3,7,11,15,19,23,27,4,8,12,16,20,24,44,45]"
  
 ### File / path where STDOUT will be written, the %J is the job id
-#BSUB -o log_files/solving_HC_%I
+#BSUB -o log_files/solving_inflow_%I
  
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute,
 ### that means for 80 minutes you could also use this: 1:20
-#BSUB -W 20:00
+#BSUB -W 5:00
  
 ### Request memory you need for your job in MB
-#BSUB -M 8000
+#BSUB -M 5000
  
 ### Change to the work directory
 cd /home/ns179556/SBP_1D/
@@ -23,7 +23,7 @@ module load matlab
  
  
 # start non-interactive batch job
-matlab -singleCompThread -nodisplay -nodesktop -nosplash -logfile log_files/solve_hc_1x3v_$LSB_JOBINDEX.log <<EOF
-run solve_HC_1x3v($LSB_JOBINDEX);
+matlab -singleCompThread -nodisplay -nodesktop -nosplash -logfile log_files/solve_inflow_1x3v_$LSB_JOBINDEX.log <<EOF
+run solve_inflow_1x3v($LSB_JOBINDEX);
 quit();
 EOF
